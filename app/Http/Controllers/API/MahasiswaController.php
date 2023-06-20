ca2c8299db7a67e7065354d50f93a06195b59e2b
Sugeng Kurniawan <sugengkrnwn95@gmail.com>
2023-06-20 14:43:32 +0700

    sample rest api

---
+++ app/Http/Controllers/API/MahasiswaController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ApiFormatter;
+use App\Http\Controllers\Controller;
+use App\Models\Mahasiswa;
+use Exception;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Mahasiswa::all();
+
+        if($data){
+            return ApiFormatter::createApi(200,'Succes', $data);
+        }else{
+            return ApiFormatter::createApi(400,'Failed');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'username' => 'required',
+                'address' => 'required',
+            ]);
+
+            $mahasiswa = Mahasiswa::create([
+                'username' => $request->username,
+                'address' => $request->address
+            ]);
+
+            $data = Mahasiswa::where('id', '=', $mahasiswa->id)->get();
+            if($data){
+                return ApiFormatter::createApi(200,'Succes create', $data);
+            }else{
+                return ApiFormatter::createApi(400,'Failed');
+            }
+
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(400,'Failed');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = Mahasiswa::where('id', '=', $id)->get();
+        if($data){
+            return ApiFormatter::createApi(200,'Succes', $data);
+        }else{
+            return ApiFormatter::createApi(400,'Failed');
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $request->validate([
+                'username' => 'required',
+                'address' => 'required',
+            ]);
+
+            $mahasiswa = Mahasiswa::findOrfail($id);
+
+            $mahasiswa->update([
+                'username' => $request->username,
+                'address' => $request->address
+            ]);
+
+            $data = Mahasiswa::where('id', '=', $mahasiswa->id)->get();
+            if($data){
+                return ApiFormatter::createApi(200,'Succes update', $data);
+            }else{
+                return ApiFormatter::createApi(400,'Failed');
+            }
+
+        } catch (Exception $error) {
+            return ApiFormatter::createApi(400,'Failed');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $mahasiswa = Mahasiswa::findOrFail($id);
+
+        $data = $mahasiswa->delete();
+        if($data){
+            return ApiFormatter::createApi(200,'Succes Destroy data', $data);
+        }else{
+            return ApiFormatter::createApi(400,'Failed');
+        }
+    }
+}
